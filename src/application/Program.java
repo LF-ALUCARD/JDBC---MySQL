9e55dce18f896f01b8a25a6c986ea8de8949a9d6
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-03 14:11:16 -0300

    Inserindo dados pela main

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,23 @@
 package application;
 
-import java.util.List;
+import java.util.Date;
 
 import model.dao.DaoFactory;
-import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		DepartmentDao obj = DaoFactory.createDepartmentDaoJDBC();
+		SellerDao obj_1 = DaoFactory.createSellerDaoJDBC();
 		
-		List<Department> list = obj.findAll();
+		Seller seller = new Seller(null, "bot1", "bot1@gmail.com", new Date(), 5000.0, new Department(2, null));
 		
-		list.forEach(System.out::println);
+		System.out.println("=== TESTE 1 SELLER ===");
+		obj_1.insert(seller);
+		System.out.println("Dados inseridos com sucesso");
 	}
 
 }
