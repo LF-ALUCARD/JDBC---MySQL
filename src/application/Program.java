fe5a5c96032002c451d440ba2b65f7039295b337
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-05 11:01:32 -0300

    Delete dados pela main

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,7 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
@@ -13,12 +9,7 @@ public class Program {
 		
 		SellerDao obj_1 = DaoFactory.createSellerDaoJDBC();
 		
-		Seller seller = new Seller(17, "bot1", "bot1@gmail.com", new Date(), 5000.0, new Department(2, null));
-		
-		System.out.println("=== TESTE 2 SELLER ===");
-		seller.setBaseSalary(10000.00);
-		obj_1.update(seller);
-		System.out.println("Dados atualizados com sucesso");
+		obj_1.delete(17);
 	}
 
 }
